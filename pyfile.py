91c5158539ee3c425fae8c0d7c69e11fb109cc10
gauravphuyal <phuyalgaurav90@gmail.com>
2024-08-28 23:21:10 +0545

    Refactor code to load codes from a file and type them using keyboard input

--- pyfile.py
+++ pyfile.py
@@ -1,18 +1,29 @@
-import pyautogui
 import keyboard
-import pyperclip
+import time
 
-f = open('codes.txt', 'r')
-arrOfCodes = f.read().split('\n')
 
-for codes in arrOfCodes:
-    while True:
-        if keyboard.is_pressed('backspace'):
-            pyperclip.copy(codes)
-            pyautogui.hotkey("ctrl", "v")
-            pyautogui.press("enter")
-            pyautogui.press("enter", interval=5)
-            break
+def load_codes_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        print(f"Error: '{file_path}' file not found.")
+        return []
 
-print('completed.')
 
+codes = load_codes_from_file('codes.txt')
+
+
+def type_phrase():
+    if codes:
+        phrase = codes[0]
+        for char in phrase:
+            keyboard.write(char)
+            time.sleep(0.05)  # Adjust the delay as needed
+        codes.remove(phrase)
+
+
+keyboard.add_hotkey('F12', type_phrase)
+
+print("Press F12 to type a phrase.")
+keyboard.wait('esc')
