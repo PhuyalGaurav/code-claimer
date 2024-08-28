4847a4b638a03d195485e14ee1dfd23a176358b6
Manish Bagale <manish.bagale3@gmail.com>
2024-08-28 22:03:56 +0545

    added a ending note and changed to a convinient key

--- pyfile.py
+++ pyfile.py
@@ -6,6 +6,8 @@ arrOfCodes = f.read().split('\n')
 
 for codes in arrOfCodes:
     while True:
-        if keyboard.is_pressed('page up'):
+        if keyboard.is_pressed('backspace'):
             pyautogui.typewrite(codes + '\n')
             break
+
+print('completed.')
