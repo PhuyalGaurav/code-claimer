b2b4ebe71596fb76ff67eed2ecd067258c2e5cf5
Manish Bagale <manish.bagale3@gmail.com>
2024-08-30 23:19:57 +0545

    automazted the code entering proces

--- pyfile.py
+++ pyfile.py
@@ -1,18 +1,25 @@
 import pyautogui
 import keyboard
 import pyperclip
+import time
 
 f = open('codes.txt', 'r')
 arrOfCodes = f.read().split('\n')
 
-for codes in arrOfCodes:
-    while True:
-        if keyboard.is_pressed('backspace'):
+while True:
+    if keyboard.is_pressed("backspace"):
+        for codes in arrOfCodes:
             pyperclip.copy(codes)
             pyautogui.hotkey("ctrl", "v")
             pyautogui.press("enter")
-            pyautogui.press("enter", interval=5)
-            break
+            time.sleep(8)
+            pyautogui.press('z')
+            time.sleep(2)
+            pyautogui.doubleClick()
+            time.sleep(1)
+            pyautogui.hotkey('ctrl', 'a')
+            pyautogui.press("backspace")
+        break
 
 print('completed.')
 
